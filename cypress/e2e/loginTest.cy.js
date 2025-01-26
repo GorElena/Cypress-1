a18f596f45df9001686d5dce97480a3e16e52668
GorLena <zhura1977@list.ru>
2025-01-26 22:53:37 +0300

    retest commit

--- cypress/e2e/loginTest.cy.js
+++ cypress/e2e/loginTest.cy.js
@@ -1,32 +1,43 @@
-describe('template spec', () => {
-
+describe('Тесты на авторизацию сайта библиотека', () => {
+  
   beforeEach(() => {
-    cy.visit("/");
+    cy.visit('/');
   })
   
-  it('Correct login & pass', () => {
-    cy.visit('http://localhost:3000');
-    cy.get('.ml-auto > .ml-2').click();
-    cy.get('#mail').type('test@test.com');
-    cy.get('#pass').type('test');
-    cy.get('form > .ml-2').click();
-    cy.contains('Log out').should('be.visible');
-    cy.contains('Добро пожаловать test@test.com').should('be.visible');
+  it('Успешная авторизация в библиотеке', () => {
+    cy.login('test@test.com', 'test');
+    cy.contains('Добро пожаловать test@test.com').should('be.visible').true;
+    //cy.contains("Добро пожаловать").should("have.length", 1)
   })
 
-  it('Ввод пустого поля пароль ', () => {
-    cy.visit('http://localhost:3000');
-    cy.get('.ml-auto > .ml-2').click();
-    cy.get('#mail').type('test@test.com');
-    cy.get('form > .ml-2').click();
-    cy.get('#pass').then((elements) => elements[0].checkValidity()).should('be.false')
-   
+  it("Ввод пустого поля Email", () => {
+    cy.login(null, 'test');
+    cy.get('#mail').then((elements) => {
+      expect(elements[0].checkValidity()).to.be.false;
+      expect(elements[0].validationMessage).to.be.eql('Заполните это поле.');
+    })
   })
-  it('Ввод пустого поля логина ', () => {
-    cy.visit('http://localhost:3000');
-    cy.get('.ml-auto > .ml-2').click();
-    cy.get('#pass').type('test');
-    cy.get('form > .ml-2').click();
-    cy.get('#mail').then((elements) => elements[0].checkValidity()).should('be.false')
-  })
+
+  it('Ввод пустого Password', () => {
+    cy.login('test@test.com', null);
+    cy.get('#pass').then((elements) => {
+      expect(elements[0].checkValidity()).to.be.false;
+      expect(elements[0].validationMessage).to.be.eql('Заполните это поле.');
+    })
+  });
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
